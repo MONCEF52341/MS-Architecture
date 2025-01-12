26830ebdedd44e50a5cbe5d82b6b1a78c371b167
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-01-12 15:02:48 +0100

    creation d'un mapper pour student <-> studentDTO

---
+++ student/src/main/java/emsi/moncef/student/mappers/StudentMapper.java
@@ -0,0 +1,31 @@
+package emsi.moncef.student.mappers;
+
+import emsi.moncef.student.dtos.StudentDTO;
+import emsi.moncef.student.models.Student;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class StudentMapper {
+
+    public StudentDTO toDto(Student student) {
+        return new StudentDTO(
+                student.getFirstName(),
+                student.getLastName(),
+                student.getDateOfBirth(),
+                student.getEmail(),
+                student.getPhoneNumber(),
+                student.getAddress(),
+                student.getMajor(),
+                student.getYearOfStudy()
+        );
+    }
+
+    public List<StudentDTO> toDtoList(List<Student> students) {
+        return students.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+}
