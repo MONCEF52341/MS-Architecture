25806c043ecd6512eb161e997a7bd4e00078c2be
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-01-12 16:01:16 +0100

    gestion d'un edge case

--- student/src/main/java/emsi/moncef/student/DataLoader.java
+++ student/src/main/java/emsi/moncef/student/DataLoader.java
@@ -4,9 +4,12 @@ import com.github.javafaker.Faker;
 import emsi.moncef.student.enums.Major;
 import emsi.moncef.student.models.Student;
 import emsi.moncef.student.repositories.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -17,6 +20,7 @@ import java.util.Set;
 public class DataLoader implements CommandLineRunner {
 
 
+    private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
     private final Faker faker = new Faker();
     private final Random random = new Random();
     Set<String> usedSerials = new HashSet<>();
@@ -25,10 +29,15 @@ public class DataLoader implements CommandLineRunner {
     private StudentRepository studentRepository;
 
     @Override
-    public void run(String... args) throws Exception {
-        loadStudents();
+    public void run(String... args) {
+        try {
+            loadStudents();
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
+            System.exit(1);
+        }
     }
-
 
     private void loadStudents() {
         for (int i = 0; i < 100; i++) {
