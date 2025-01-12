2432f8cc32afc4cccc3fa125f8f90f4a8f106c09
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-01-12 18:11:27 +0100

    ajout de méthodes dans le repo

--- order/src/main/java/emsi/moncef/order/repositories/OrderRepository.java
+++ order/src/main/java/emsi/moncef/order/repositories/OrderRepository.java
@@ -3,5 +3,10 @@ package emsi.moncef.order.repositories;
 import emsi.moncef.order.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderNumber(String orderNumber);
+
+    void deleteByOrderNumber(String orderNumber);
 }
