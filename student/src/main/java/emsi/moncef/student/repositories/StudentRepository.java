b48851bea86c5d8be9a542657bd5ef254afef0ea
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-01-12 15:03:00 +0100

    couche repo

--- student/src/main/java/emsi/moncef/student/repositories/StudentRepository.java
+++ student/src/main/java/emsi/moncef/student/repositories/StudentRepository.java
@@ -1,7 +1,28 @@
 package emsi.moncef.student.repositories;
 
+import emsi.moncef.student.enums.Major;
 import emsi.moncef.student.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByMajor(Major major);
+
+    Student findByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
+
+    List<Student> findByYearOfStudy(int year);
+
+    List<Student> findByYearOfStudyBetween(int minYear, int maxYear);
+
+    List<Student> findByAddressContainingIgnoreCase(String address);
+
+    Student findByEmailContainingIgnoreCase(String email);
+
+    Student findByPhoneNumber(String phoneNumber);
+
+    List<Student> findByDateOfBirth(LocalDate birthdate);
+
+    List<Student> findByDateOfBirthBetween(LocalDate startDate, LocalDate endDate);
 }
