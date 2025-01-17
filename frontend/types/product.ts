78887491887a51d047f05561107f76d444b0d380
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-01-17 19:48:03 +0100

    insallation des composants UI next

---
+++ frontend/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  skuCode: string
+  name: string
+  description: string
+  price: number
+  stockQuantity: number
+  category: string
+  manufacturer: string
+  isAvailable: boolean
+}
+
