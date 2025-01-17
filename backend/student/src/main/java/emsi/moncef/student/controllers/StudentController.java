79533d8bb26154f8dad2bb82c43b473ead3877c2
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-01-17 19:47:17 +0100

    modif du cors de 5173 à 3000

--- backend/student/src/main/java/emsi/moncef/student/controllers/StudentController.java
+++ backend/student/src/main/java/emsi/moncef/student/controllers/StudentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
